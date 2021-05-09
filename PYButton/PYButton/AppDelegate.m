5a745be0bff2563bbc9835662c7be948bdbc4c4c
ZQ <zq@ZQdeMacBook-Air.local>
2021-05-09 16:20:08 +0800

    回滚操作

--- PYButton/PYButton/AppDelegate.m
+++ PYButton/PYButton/AppDelegate.m
@@ -17,7 +17,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    //增加一行注释
     return YES;
 }
 
